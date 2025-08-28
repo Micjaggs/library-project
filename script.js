f2104bfab9968a5ee9d35de2f5b2b5728d464496
Mic <126879111+Micjaggs@users.noreply.github.com>
2025-08-28 17:06:49 +1000

    add library logic

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const bookPages = document.getElementById('pages')
 const isRead = document.getElementById('read')
 const submit = document.getElementById('submit')
 const bookList = document.getElementById('library-list')
+const form = document.getElementById('book-input')
 
 // Constructor function to create books
 
@@ -29,9 +30,69 @@ function addBook(title, author, pages, isRead) {
 
 // Function to clear Library before re-rendering
 
-function clearLibrary(el) {
-    while (el.fistChild) {
+function clearElement(el) {
+    while (el.firstChild) {
         el.removeChild(el.firstChild)
     }
 }
+
+// Function to render a book card
+
+function renderBookCard(book) {
+    const card = document.createElement('div');
+    card.classList.add('book-card');
+    card.dataset.id = book.id;
+
+    const titleEl = document.createElement('h3');
+    titleEl.textContent = book.title;
+    card.appendChild(titleEl);
+
+    const authorEl = document.createElement('p');
+    authorEl.textContent = `Author: ${book.author}`;
+    card.appendChild(authorEl);
+
+    const pagesEl = document.createElement('p');
+    pagesEl.textContent = `Pages: ${book.pages}`;
+    card.appendChild(pagesEl);
+
+    const isReadEl = document.createElement('p');
+    isReadEl.textContent = `Read: ${book.isRead}`;
+    card.appendChild(isReadEl);
+
+    const toggleBtn = document.createElement('button');
+    toggleBtn.textContent = "Toggle Read";
+    toggleBtn.classList.add('toggle-btn');
+    card.appendChild(toggleBtn);
+
+    const rmvBtn = document.createElement('button');
+    rmvBtn.textContent = 'Remove Book';
+    rmvBtn.classList.add('rmv-btn');
+    card.appendChild(rmvBtn);
+
+    return card;
+}
+
+// Function to render the Library
+
+function renderLibrary() {
+    clearElement(bookList);
+    myLibrary.forEach((book) => {
+        const card = renderBookCard(book);
+        bookList.appendChild(card)
+    });
+}
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault(); //don't reload the page
+
+    const title = bookTitle.value.trim();
+    const author = bookAuthor.value.trim();
+    const pages = Number(bookPages.value)
+    const read = isRead.checked;
+
+    addBook(title, author, pages, read);
+    renderLibrary();
+    form.reset();
+    bookTitle.focus();
+})
 
